272d599afd9a754ae39fb8c1138f463f0a8517ea
Artem Kuzmin <artem.m.kuzmin@gmail.com>
2026-09-12 02:40:48 +0300

    add UART interrupts

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -9,12 +9,12 @@
 
 use defmt::info;
 use esp_backtrace as _;
-use esp_hal::analog::adc;
+use esp_hal::analog::adc::{Adc, AdcConfig, Attenuation};
 use esp_hal::clock::CpuClock;
 use esp_hal::gpio::{Input, InputConfig, Level, Output, OutputConfig, Pull};
 use esp_hal::main;
 use esp_hal::time::{Duration, Instant};
-use esp_hal::uart::{Config, RxConfig, TxConfig, Uart};
+use esp_hal::uart::{AtCmdConfig, Config, RxConfig, Uart, UartInterrupt};
 use esp_println as _;
 
 esp_bootloader_esp_idf::esp_app_desc!();
@@ -36,8 +36,8 @@ fn main() -> ! {
     // let exp_p5_15 = peripherals.GPIO2;
     // let lora_nreset = peripherals.GPIO3;
     // let battery_voltage = peripherals.GPIO4;
-    // let gnss_txd = peripherals.GPIO5;
-    // let gnss_rxd = peripherals.GPIO6;
+    let gnss_rxd = peripherals.GPIO5;
+    let gnss_txd = peripherals.GPIO6;
     // let gnss_1pps = peripherals.GPIO7;
     // let i2c_sda = peripherals.GPIO8;
     // let i2c_sdl = peripherals.GPIO9;
@@ -58,8 +58,8 @@ fn main() -> ! {
     // let lora_ldo_en = peripherals.GPIO40;
     // let fan_ctrl = peripherals.GPIO41;
     // let ext_p5_10 = peripherals.GPIO42;
-    let ext_txd = peripherals.GPIO43;
-    let ext_rxd = peripherals.GPIO44;
+    //let ext_txd = peripherals.GPIO43;
+    //let ext_rxd = peripherals.GPIO44;
     // let ext_p5_8 = peripherals.GPIO45;
     // let ext_p6_4 = peripherals.GPIO46;
     // let ext_p5_6 = peripherals.GPIO47;
@@ -79,29 +79,72 @@ fn main() -> ! {
     let _ = peripherals.GPIO37;
 
     let mut led = Output::new(user_led, Level::High, OutputConfig::default());
-    let temp = Adc::new();
-    let temp = Input::new(temp_samp, InputConfig::default().with_pull(Pull::None));
+
+    let mut adc1_config = AdcConfig::new();
+    let mut pin = adc1_config.enable_pin(temp_samp, Attenuation::_11dB);
+    let mut adc1 = Adc::new(peripherals.ADC1, adc1_config);
+
     let button = Input::new(user_button, InputConfig::default().with_pull(Pull::Up));
 
-    let mut uart1 = Uart::new(
-        peripherals.UART1,
-        Config::default()
-            .with_baudrate(115200)
-            .with_rx(RxConfig::default())
-            .with_tx(TxConfig::default()),
-    )
-    .unwrap();
+    let mut uart = Uart::new(peripherals.UART0, Config::default().with_baudrate(9600))
+        .unwrap()
+        .with_rx(gnss_rxd)
+        .with_tx(gnss_txd);
+    uart.set_interrupt_handler(interrupt_handler);
+    critical_section::with(|cs| {
+        uart.set_at_cmd(AtCmdConfig::default().with_cmd_char(b'#'));
+        uart.listen(UartInterrupt::AtCmd | UartInterrupt::RxFifoFull);
 
-    //   .with_rx(gnss_rxd).with_tx(gnss_txd)
+        SERIAL.borrow_ref_mut(cs).replace(uart);
+    });
 
     loop {
-        //uart1.write(b"Hello uart1!\r\n").unwrap();
-        let temp = temp.peripheral_input().level();
+        let temp = adc1.read_blocking(&mut pin);
         info!("Temp: {}", temp);
         let button = button.peripheral_input().level();
         info!("Button: {}", button);
         led.toggle();
         let delay_start = Instant::now();
         while delay_start.elapsed() < Duration::from_millis(500) {}
+    }
+
+    use core::cell::RefCell;
+
+    use critical_section::Mutex;
+    static SERIAL: Mutex<RefCell<Option<Uart<esp_hal::Blocking>>>> = Mutex::new(RefCell::new(None));
+
+    #[esp_hal::handler]
+    fn interrupt_handler() {
+        critical_section::with(|cs| {
+            let mut serial = SERIAL.borrow_ref_mut(cs);
+            if let Some(serial) = serial.as_mut() {
+                let mut buf = [0u8; 256];
+                if let Ok(cnt) = serial.read_buffered(&mut buf) {
+                    info!("Read {} bytes", cnt);
+                    info!("Read buf {}", get_ascii_str(&buf));
+                }
+
+                let pending_interrupts = serial.interrupts();
+                info!(
+                    "Interrupt AT-CMD: {} RX-FIFO-FULL: {}",
+                    pending_interrupts.contains(UartInterrupt::AtCmd),
+                    pending_interrupts.contains(UartInterrupt::RxFifoFull),
+                );
+
+                serial.clear_interrupts(UartInterrupt::AtCmd | UartInterrupt::RxFifoFull);
+            }
+        });
+    }
+    fn get_ascii_str<'a>(buffer: &'a [u8]) -> Result<&'a str, ()> {
+        for byte in buffer.into_iter() {
+            if byte >= &128 {
+                return Err(());
+            }
+        }
+        Ok(unsafe {
+            // This is safe because we verified above that it's a valid ASCII
+            // string, and all ASCII strings are also UTF8 strings
+            core::str::from_utf8_unchecked(buffer)
+        })
     }
 }
